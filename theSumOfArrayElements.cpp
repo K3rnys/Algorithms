c074551ac46d36cbd84571adb14e1e7a1281a176
K3rnys <rainykernys@gmail.com>
2024-04-01 13:55:46 +0300

    Ваше сообщение коммита

---
+++ theSumOfArrayElements.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+
+
+int sum(std::vector<int> data) {				// recursive function		
+	// recursive case
+	if (!data.empty()) {							
+		int mem = data[0];						// remember the first element
+		data.erase(data.begin());				// erase the first element
+		return sum(data) + mem;					// add the mem variable to the sum
+	}
+	// base case
+	return 0;										
+}
+
+
+void main() {
+	std::vector<int> mas = { 3, 4, 5, 6 };		// for example 3 + 4 + 5 + 6 = 18
+	std::cout << sum(mas);
+}
+
+
+// in this case, a call stack overflow may occur
